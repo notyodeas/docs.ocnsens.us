e3b2683a9f3d68deab2fd87305befe8563bbbcd4
notyodeas <notyodeasnotpurples@gmail.com>
2025-05-22 22:51:39 +0200

    Update server.dart

--- bin/server.dart
+++ bin/server.dart
@@ -23,7 +23,7 @@ Response _echoHandler(Request request) {
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
-  final path = '/root/docs.ocnsens.us/specs/ocnnn.yaml';
+  final path = '/specs/ocnnn.yaml';
   // Configure a pipeline that logs requests.
   // final handler = Pipeline().addMiddleware(SwaggerUI()).addHandler(_router);
   final handler = SwaggerUI(path, title: 'proofofdivisionexplained');
